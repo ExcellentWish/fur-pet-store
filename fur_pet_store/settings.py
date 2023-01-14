91c275ddc6bc15c96d9f3c450decf1de5ef6d323
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-14 13:59:21 +0000

    Added add to bag functionality

--- fur_pet_store/settings.py
+++ fur_pet_store/settings.py
@@ -113,7 +113,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'bag.contexts.bag_contents',
+                'bag.contexts_processor.bag_contents',
             ],
         },
     },
