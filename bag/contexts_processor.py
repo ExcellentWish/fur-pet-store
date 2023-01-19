3321bd207bbac20d662ac887428b898a2a6d1ab0
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-19 09:58:17 +0000

    Fix toasts_success and info for base.html. Update context_processor and create __init__.py for bag app

--- bag/contexts_processor.py
+++ bag/contexts_processor.py
@@ -9,14 +9,14 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
+    for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += quantity * product.price
-            product_count += quantity
+            total += item_data * product.price
+            product_count += item_data
             bag_items.append({
                 'item_id': item_id,
-                'quantity': quantity,
+                'quantity': item_data,
                 'product': product,
             })
         else:
@@ -26,7 +26,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
@@ -37,9 +37,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
