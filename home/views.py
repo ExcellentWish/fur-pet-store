30be4f9fb42570b76a58da7344101452dfc0085f
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-19 11:38:36 +0000

    Add terms and conditions, Privacy policy pages. Fix footer links in base.html. Add animal_product basic elements

--- home/views.py
+++ home/views.py
@@ -10,3 +10,11 @@ def about(request):
     """ A view to return the about page """
     return render(request, 'home/about.html')
 
+def privacy_policy(request):
+    """ A view to return the about page """
+    return render(request, 'home/privacy_policy.html')
+
+
+def terms_conditions(request):
+    """ A view to return the about page """
+    return render(request, 'home/terms_conditions.html')
