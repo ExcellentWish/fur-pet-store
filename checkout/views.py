729b314db0ebe77499aa8be7256c7efaaa69672a
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-21 16:14:11 +0000

    Set up stripe. Add stripe_elelments.js

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key':'pk_test_51MI7DkFTdUdPuHxjrR8IJX2WnvNt1jwZvGyRFB9UicmMovQqFAVtCjs20vpknq36CjKSnlrba8vwaWKVGVhEa9yh00oH9UFgjF',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
