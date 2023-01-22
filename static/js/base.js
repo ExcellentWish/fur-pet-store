e8264db4a4d69d6c0faa48d6125676f9039ef494
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-22 11:45:59 +0000

    Create base.js for js on base.html. Amend text to bootstrap 5

---
+++ static/js/base.js
@@ -0,0 +1,20 @@
+let toastElList = [].slice.call(document.querySelectorAll('.toast'))
+let toastList = toastElList.map(function (toastEl) {
+    let option = {
+    animation: true,
+    autohide: false,
+    delay: 5000,
+    }
+    let bsToast = new bootstrap.Toast(toastEl, option)
+    bsToast.show();
+})
+
+$("#copyright").text(new Date().getFullYear());
+
+
+$("#footer-container .body").hide();
+$("#footer-container .button").click(function () {
+    $(this).next("#footer-container div").slideToggle(400);
+    $(this).toggleClass("expanded")
+});
+  
