3a77861f1a7649ae8f66a798d6bc276f29863506
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-22 13:49:00 +0000

    Set up webhooks.py. Add functions to StripeWH_handler in webhook_handeler to handle payment events

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -11,3 +11,15 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}', status=200
         )
+    
+    def handle_payment_intent_succeeded(self, event):
+        # handle the payment_intent.succeeded webhook event 
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}', status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        # handle the payment_intent.payment_failed webhook event 
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}', status=200
+        )
