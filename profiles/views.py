ab6b845ad79b9305a5f4e38543c897ae41e9a2c7
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-22 16:02:53 +0000

    Set up for profile order history

--- profiles/views.py
+++ profiles/views.py
@@ -9,22 +9,9 @@ from .forms import UserProfileForm
 
 def profile(request):
     # display the user's profile
-    profile = get_object_or_404(UserProfile, user=request.user)
-
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
-
     template = 'profiles/profile.html'
     context = {
-        'form': form,
-        'orders': orders,
-        'on_profile_page': True,
+        'profile': profile,
     }
 
     return render(request, template, context)
