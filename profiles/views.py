050904d0cb9e6e908c0f5c7026e9c9c10731c18e
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-22 15:27:20 +0000

    Test profile.html

--- profiles/views.py
+++ profiles/views.py
@@ -4,24 +4,14 @@ from .models import UserProfile
 from .forms import UserProfileForm
 
 # Create your views here.
+
+
+
 def profile(request):
-    # Display the user's profile.
-    profile = get_object_or_404(UserProfile, user=request.user)
-
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
-
+    # display the user's profile
     template = 'profiles/profile.html'
     context = {
-        'form': form,
-        'orders': orders,
-        'on_profile_page': True,
+        'profile': profile,
     }
 
     return render(request, template, context)
