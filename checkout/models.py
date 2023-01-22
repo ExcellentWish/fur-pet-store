f59c4edae26362c1aaf88f1aa3de676a06e07e5e
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-22 11:21:10 +0000

    Amend base.html corejs.Create static/checkout folder and move css and js into folder

--- checkout/models.py
+++ checkout/models.py
@@ -1,17 +1,14 @@
 import uuid
+
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-from products.models import Product
-from django_countries.fields import CountryField
-from crispy_forms.utils import render_crispy_form
-from profiles.models import UserProfile
 
-# Create your models here.
+from products.models import Product
+
+
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
-                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -30,13 +27,13 @@ class Order(models.Model):
         Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
-    
+
     def update_total(self):
         """
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']  or 0
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
