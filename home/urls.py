a4763ef74da639f06e63fc2c2fbfd3883909682d
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-23 17:25:29 +0000

    Add contact_us.html, url and view

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('privacy_policy', views.privacy_policy, name='privacy_policy'),
     path('terms_conditions', views.terms_conditions, name='terms_conditions'),
-    
+    path('contact_us', views.ContactPage.as_view(), name='contact_us'),    
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .forms import ContactForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -18,3 +21,24 @@ def privacy_policy(request):
 def terms_conditions(request):
     """ A view to return the about page """
     return render(request, 'home/terms_conditions.html')
+
+class ContactPage(View):
+
+    def get(self, request, *args, **kwargs):
+        contact_form = ContactForm()
+        template = 'home/contact_us.html'
+        return render(request, 'home/contact_us.html', {'contact_form': contact_form})
+
+
+    def contact_form(request):
+
+        if request.method == 'POST':
+            contact_form = ContactForm(request.POST)
+
+            if form.is_valid():
+                return render(request, 'home/contact_us.html', {'contact_form': contact_form})
+
+            else:
+                contact_form = ContactForm()
+
+            return render(request, 'home/contact_us.html', {'contact_form': contact_form})
