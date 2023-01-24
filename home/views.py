f39ab9e7a42d61ed795e5c85f928f56a76a3ed92
Nigel Doheny <nigel.doheny93@gmail.com>
2023-01-24 10:52:56 +0000

    Add send message function for contact_us

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic, View
+from django.core.mail import send_mail
 from .forms import ContactForm
 from django.http import HttpResponseRedirect
 
@@ -22,12 +23,21 @@ def terms_conditions(request):
     """ A view to return the about page """
     return render(request, 'home/terms_conditions.html')
 
+def send_message(request, contact_form):
+    # Function to send email after contact form submitted
+    customer_name = contact_form.cleaned_data['name']
+    email_from = contact_form.cleaned_data['email']
+    subject = (f'Message from {customer_name}, {email_from}')
+    message = contact_form.cleaned_data['message']
+    recipient_list = [settings.EMAIL_HOST_USER]
+    send_mail(subject, message, email_from, recipient_list)
+
 class ContactPage(View):
 
     def get(self, request, *args, **kwargs):
         contact_form = ContactForm()
         template = 'home/contact_us.html'
-        return render(request, 'home/contact_us.html', {'contact_form': contact_form})
+        return render(request, template, {'contact_form': contact_form})
 
 
     def contact_form(request):
