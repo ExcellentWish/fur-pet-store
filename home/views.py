a6b37c989f3f87a2b125e4e52d5c02406dc9598c
Nigel Doheny <nigel.doheny93@gmail.com>
2023-02-02 10:46:45 +0000

    Remove handlers from home.views. Amend Css for notfound backgorund image

--- home/views.py
+++ home/views.py
@@ -76,12 +76,3 @@ class ContactPage(View):
 
             return render(request, template, {'contact_form': contact_form})
 
-
-
-def handler404(request, exception):
-    """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
-
-def handler500(request):
-    """ Error Handler 500- Page Not Found """
-    return render(request, "errors/500.html", status=500)
