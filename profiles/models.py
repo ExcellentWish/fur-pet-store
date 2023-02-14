eb58e3b9a04f9f63b31d0cb4116f07e374bc60c0
Nigel Doheny <nigel.doheny93@gmail.com>
2023-02-14 11:22:29 +0000

    Import Product model to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_countries.fields import CountryField
+from products.models import Product
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -33,6 +34,8 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
+
+
 
 class UserWishlist(models.Model):
     """ wishlist model """
